7e42c0e9547dd863c9e61e390124f68222b7b2d2
Cyberthal <Texas.Cyberthal@gmail.com>
2016-10-18 19:33:58 +0800

    added boxes macro for checklists

--- functions/cb-macros.el
+++ functions/cb-macros.el
@@ -11,4 +11,11 @@
     )
   )
 
+;; Org-Mode: "boxes" Advance down org-mode checklist with C-S-n
+(fset 'boxes
+      (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("" 0 "%d")) arg)))
+;; binding it to a handy key, since as a command it won't macro repeat
+(global-set-key (kbd "C-S-n") 'boxes)
+
+
 (provide 'cb-macros)
