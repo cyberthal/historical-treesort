558dd284dbed99c8321547cbd476513bd3479c8a
leo3 <leo3@imac.local>
2019-06-15 23:34:44 +0800

    deploy converts link name to lowercase

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LINK_NAME=${PWD##*/}
 
-rm -rf ~/.emacs.d/private/cyborganize
-ln -s $SCRIPT_DIR ~/.emacs.d/private/
+link_name=$(echo "$LINK_NAME" | tr '[:upper:]' '[:lower:]')
+
+ln -sf $SCRIPT_DIR ~/.emacs.d/private/$link_name
 
