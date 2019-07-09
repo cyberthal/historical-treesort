0e4536e2addc8fbc5f1125a83dff387fd88b2c6d
leo3 <texas.cyberthal@gmail.com>
2019-07-09 18:07:07 +0800

    whitespace edit

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LINK_NAME=${PWD##*/}
 
+LINK_NAME=${PWD##*/}
 link_name=$(echo "$LINK_NAME" | tr '[:upper:]' '[:lower:]')
 
 ln -sf $SCRIPT_DIR ~/.emacs.d/private/$link_name
-
