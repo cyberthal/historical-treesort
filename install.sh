a77afab423f771c2e3d13c72105d9824e5cc4546
leo3 <texas.cyberthal@gmail.com>
2019-07-15 22:13:44 +0800

    add mkdir for chemacs spacemacs dir

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LINK_NAME="cyborganize"
 
+mkdir ~/.spacemacs.d/
 rm -f ~/.spacemacs.d/$LINK_NAME
 ln -s $SCRIPT_DIR ~/.spacemacs.d/$LINK_NAME
