167322f07284740944a48cdd980e30047b3b04e8
leo3 <texas.cyberthal@gmail.com>
2019-07-16 01:20:38 +0800

    fix layer dir location

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LINK_NAME="cyborganize"
 
-mkdir ~/Spacemacs/
-rm -f ~/Spacemacs/$LINK_NAME
-ln -s $SCRIPT_DIR ~/Spacemacs/$LINK_NAME
+mkdir -p ~/Spacemacs/layers/
+rm -f ~/Spacemacs/layers/$LINK_NAME
+ln -s $SCRIPT_DIR ~/Spacemacs/layers/$LINK_NAME
