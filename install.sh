6aace6479fb6112f9a5b15f59a2f0d0da45ca51a
leo3 <texas.cyberthal@gmail.com>
2019-07-15 21:50:55 +0800

    edit paths for chemacs

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LINK_NAME="cyborganize"
 
-rm -f ~/.emacs.d/private/$LINK_NAME
-ln -s $SCRIPT_DIR ~/.emacs.d/private/$LINK_NAME
+rm -f ~/.spacemacs.d/$LINK_NAME
+ln -s $SCRIPT_DIR ~/.spacemacs.d/$LINK_NAME
