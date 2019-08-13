e6aee15deb1f88c48eb2af85ae1020727ad9eb69
leo3 <texas.cyberthal@gmail.com>
2019-08-13 17:04:48 +0800

    deleted redundant layer files
    now that it's a package

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-LINK_NAME="treesort"
-
-rm -f ~/.emacs.d/private/$LINK_NAME
-ln -s $SCRIPT_DIR ~/.emacs.d/private/$LINK_NAME
