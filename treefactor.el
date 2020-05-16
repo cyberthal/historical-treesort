48138c2d7bbf817f3d4efa64aea90a6f29add87f
leo3 <texas.cyberthal@gmail.com>
2020-05-17 00:21:21 +0800

    fix critical heading refile bug that caused data loss

--- treefactor.el
+++ treefactor.el
@@ -196,9 +196,9 @@ Assume a polished document will have a level-1 near the top."
 
   (goto-char (point-min))
   (condition-case nil
-      (re-search-forward "^* ")       ; Search for a level-1 headline.
+      (progn (re-search-forward "^* ")       ; Search for a level-1 headline.
        (goto-char (point-at-bol))
-       (insert treefactor-object-text)
+       (insert treefactor-object-text))
     (error (treefactor-insert-to-end-of-buffer)))
   (save-buffer)
   (treefactor-text-inserted-to-buffer-path-message))
