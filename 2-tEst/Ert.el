f801b813d41248bdb22d50f1eafdd8cb336b789e
leo3 <texas.cyberthal@gmail.com>
2021-03-02 00:09:33 +0800

    clean up formatting

--- 2-tEst/Ert.el
+++ 2-tEst/Ert.el
@@ -7,6 +7,5 @@
          (treefactor-org-id-extra-dir (concat default-directory "Scratch/Org/Extra")))
        (should
         (equal '(("/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Agenda/1/2/3.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Agenda/1/2.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Agenda/1.org")
-                ("/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1/2/3.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1/2.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1.org"))
-               (progn (treefactor-refresh-org-search-scope) (list org-agenda-files org-id-extra-files)))
-        )))
+                 ("/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1/2/3.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1/2.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1.org"))
+               (progn (treefactor-refresh-org-search-scope) (list org-agenda-files org-id-extra-files))))))
