1af7ae48141ea8c36790ac9c052dbfbb0bae6bfe
leo3 <texas.cyberthal@gmail.com>
2021-03-03 11:34:29 +0800

    write pseudocode for treefactor-up
    and finish scratch -> mock rename

--- 2-tEst/Ert.el
+++ 2-tEst/Ert.el
@@ -1,10 +1,21 @@
 ;;; treefactor ERT test file     -*- lexical-binding: t; -*-
 
+(ert-deftest treefactor-up ()
+    (REMOVE TARGET DIR, WITH ERROR HANDLING)
+    (COPY TARGET DIR)
+  (save-window-excursion
+    (find-file (concat default-directory "Mock/Treefactor/Up/File/A/Target/1"))
+    (MOVE POINT TO OBJECT)
+    (TREEFACTOR-UP)
+    (DIFF TARGET AND END DIRS, WITH SHOULD)
+    )
+  )
+
 (ert-deftest treefactor-org-refactor-heading ()
   (save-window-excursion
   (switch-to-buffer "treefactor-org-refactor-heading-test.org" t)
   (erase-buffer)
-  (insert-file-contents (concat default-directory "Scratch/Treefactor/Org-refactor-heading/Start.org"))
+  (insert-file-contents (concat default-directory "Mock/Treefactor/Org-refactor-heading/Start.org"))
   (goto-char (search-forward "**** target heading"))
   (delete-other-windows)
   (org-mode)
@@ -23,6 +34,6 @@
          (treefactor-org-agenda-dir (concat default-directory "Mock/Org/Agenda"))
          (treefactor-org-id-extra-dir (concat default-directory "Mock/Org/Extra")))
     (should
-     (equal '(("/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Agenda/1/2/3.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Agenda/1/2.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Agenda/1.org")
-              ("/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1/2/3.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1/2.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Scratch/Org/Extra/1.org"))
+     (equal '(("/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Mock/Org/Agenda/1/2/3.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Mock/Org/Agenda/1/2.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Mock/Org/Agenda/1.org")
+              ("/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Mock/Org/Extra/1/2/3.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Mock/Org/Extra/1/2.org" "/Users/leo3/1-Mansort/2-Other-text/Git-/Hub/Emacs/My/Treefactor/Dev/2-tEst/Mock/Org/Extra/1.org"))
             (progn (treefactor-refresh-org-search-scope) (list org-agenda-files org-id-extra-files))))))
